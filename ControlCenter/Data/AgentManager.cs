759c826f9db61c0b376d8b0ef04d71434521c76e
David Fowler <davidfowl@gmail.com>
2022-12-03 22:31:01 -0800

    Something something better encapsulation

--- ControlCenter/Data/AgentManager.cs
+++ ControlCenter/Data/AgentManager.cs
@@ -2,32 +2,40 @@
 using ControlCenter.Hubs;
 using Microsoft.AspNetCore.SignalR;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 
 public class AgentManager
 {
-    private IHubContext<AgentHub, IAgent> _hubContext;
+    private readonly IHubContext<AgentHub, IAgent> _hubContext;
+    private readonly ObservableCollection<(string, IAgent)> _agents = new();
 
     public AgentManager(IHubContext<AgentHub, IAgent> hubContext)
     {
         _hubContext = hubContext;
     }
 
-    public ObservableCollection<(string, IAgent)> Agents { get; } = new();
+    public IEnumerable<(string, IAgent)> Agents => _agents;
+
+    public event NotifyCollectionChangedEventHandler StateChanged
+    {
+        add => _agents.CollectionChanged += value;
+        remove => _agents.CollectionChanged -= value;
+    }
 
     public void Add(string connectionId)
     {
-        lock (Agents)
+        lock (_agents)
         {
-            Agents.Add((connectionId, _hubContext.Clients.Client(connectionId)));
+            _agents.Add((connectionId, _hubContext.Clients.Client(connectionId)));
         }
     }
 
     public void Remove(string connectionId)
     {
-        lock (Agents)
+        lock (_agents)
         {
-            var item = Agents.FirstOrDefault(a => a.Item1 == connectionId);
-            Agents.Remove(item);
+            var item = _agents.FirstOrDefault(a => a.Item1 == connectionId);
+            _agents.Remove(item);
         }
     }
 }
